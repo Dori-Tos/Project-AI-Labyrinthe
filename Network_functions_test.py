ab887c67bcc4b8f530affffa5fbb03b68584065f
Dorian Smaelen <dorian.smaelen@hotmail.com>
2023-03-27 10:11:09 +0200

    added testing

---
+++ Network_functions_test.py
@@ -0,0 +1,16 @@
+import Network_functions
+
+def test_inscription():
+    pass
+
+def test_receiver():
+    pass
+
+def test_ping_pong ():
+    pass
+
+def testgiveup ():
+    pass
+
+def test_move():
+    pass
