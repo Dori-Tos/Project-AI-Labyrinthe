8918a170cd7ce6e234b558e061dc3dd680cb81d7
Dorian Smaelen <dorian.smaelen@hotmail.com>
2023-03-28 12:53:29 +0200

    added tests

--- Network_functions.py
+++ Network_functions.py
@@ -3,13 +3,14 @@ import threading
 import json
 
 address = ('localhost', 3000)
+serverAddress = ('0.0.0.0', 3000)
 request = "subscribe"
 port = 6942
 name = "AI_of_the_dead"
 matricules = ["22325","21006"]
 state = {}
 
-def inscription():
+def inscription(address):
     with socket.socket() as s:
         s.connect(address)
         s.send(json.dumps({"request": request, "port": port, "name": name, "matricules": matricules}).encode())
@@ -17,6 +18,9 @@ def inscription():
 def receiver():
     while True:
         with socket.socket() as s:
+            s.bind(serverAddress)
+            s.listen()
+            client, address = s.accept()
             received = json.loads(s.recv(2048).decode()) #str?
             if received.get("response")=="ok":
                 print("Successful inscription")
@@ -31,17 +35,17 @@ def receiver():
 
 thread = threading.Thread(target = receiver, daemon = True)
 
-def ping_pong ():
+def ping_pong(address):
     with socket.socket() as s:
         s.connect(address)
         s.send(json.dumps({"response": "pong"}).encode())
 
-def giveup ():
+def giveup(address):
     with socket.socket() as s:
         s.connect(address)
         s.send(json.dumps({"response": "giveup"}).encode())
 
-def move(the_move_played):
+def move(address, the_move_played):
     with socket.socket() as s:
         s.connect(address)
         s.send(json.dumps({"response": "move", "move": the_move_played, "message": "I'm comming !"}).encode())
--- Network_functions_test.py
+++ Network_functions_test.py
@@ -10,9 +10,11 @@ def test_inscription():
         serverAddress = ('0.0.0.0', 3000)
         s.bind(serverAddress)
         s.listen()
+        address = ('localhost', 3000)
+        thread = threading.Thread(target = Network_functions.inscription, daemon = True, args=(address,))
+        thread.start()
         client, address = s.accept()
         message = client.recv(2048).decode()
-        address = ('localhost', 3000)
         request = Network_functions.request
         port = Network_functions.port
         name = Network_functions.name
@@ -21,13 +23,53 @@ def test_inscription():
         assert message_sent == message
 
 def test_receiver():
-    pass
+    with socket.socket() as s:
+        address = ('localhost', 3000)
+        thread = threading.Thread(target = Network_functions.receiver, daemon = True)
+        s.connect(address)
+        s.send(json.dumps({"players": ["LUR", "HSL"],"current": 0,"positions": [6, 47],"target": 3,
+                            "remaining": 4,"tile":None,"board":None}).encode())  
+        message_sent = json.dumps({"players": ["LUR", "HSL"],"current": 0,"positions": [6, 47],"target": 3,
+                            "remaining": 4,"tile":None,"board":None})
+        
+        assert message_sent == received
 
 def test_ping_pong ():
-    pass
+    with socket.socket() as s:
+        serverAddress = ('0.0.0.0', 3000)
+        s.bind(serverAddress)
+        s.listen()
+        address = ('localhost', 3000)
+        thread = threading.Thread(target = Network_functions.ping_pong, daemon = True, args=(address,))
+        thread.start()
+        client, address = s.accept()
+        message = client.recv(2048).decode()
+        message_sent = json.dumps({"response": "pong"})
+        assert message_sent == message
 
 def test_giveup ():
-    pass
+    with socket.socket() as s:
+        serverAddress = ('0.0.0.0', 3000)
+        s.bind(serverAddress)
+        s.listen()
+        address = ('localhost', 3000)
+        thread = threading.Thread(target = Network_functions.giveup, daemon = True, args=(address,))
+        thread.start()
+        client, address = s.accept()
+        message = client.recv(2048).decode()
+        message_sent = json.dumps({"response": "giveup"})
+        assert message_sent == message
 
 def test_move():
-    pass
+    with socket.socket() as s:
+        serverAddress = ('0.0.0.0', 3000)
+        s.bind(serverAddress)
+        s.listen()
+        address = ('localhost', 3000)
+        the_move_played = None
+        thread = threading.Thread(target = Network_functions.move, daemon = True, args=(address, the_move_played))
+        thread.start()
+        client, address = s.accept()
+        message = client.recv(2048).decode()
+        message_sent = json.dumps({"response": "move", "move": the_move_played, "message": "I'm comming !"})
+        assert message_sent == message
