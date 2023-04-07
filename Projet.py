4c0ff9a63072e9012aee81927b049a78c10c7aa1
Maxime Thiry Faulkner <2235@ecam.be>
2023-04-07 17:22:54 +0100

    Ajout d'un board de test

--- Projet.py
+++ Projet.py
@@ -162,6 +162,358 @@ board = {
 	"item" : 6
 	},
 }
+tlie2={
+	"N" : True,
+	"E" : True,
+	"S" : False,
+	"W" : True,
+	"item" : 24
+}
+board2={
+	0:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	1:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	2:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : True,
+		"item" : 1
+	},
+	3:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	4:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : True,
+		"item" : 2
+	},
+	5:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	6:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 0
+	},
+	7:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 13
+	},
+	8:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	9:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 15
+	},
+	10:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 0
+	},
+	11:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : False,
+		"item" : 0
+	},
+	12:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 16
+	},
+	13:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : False,
+		"item" : 0
+	},
+	14:{
+		"N" : True,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 3
+	},
+	15:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 0
+	},
+	16:{
+		"N" : True,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 4
+	},
+	17:{
+		"N" : True,
+		"E" : False,
+		"S" : False,
+		"W" : True,
+		"item" : 17
+	},
+	18:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : True,
+		"item" : 5
+	},
+	19:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	20:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 6
+	},
+	21:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 0
+	},
+	22:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : False,
+		"item" : 16
+	},
+	23:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : True,
+		"item" : 22
+	},
+	24:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	25:{
+		"N" : True,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 20
+	},
+	26:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : False,
+		"item" : 14
+	},
+	27:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 23
+	},
+	28:{
+		"N" : True,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 7
+	},
+	29:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	30:{
+		"N" : True,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 8
+	},
+	31:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : True,
+		"item" : 21
+	},
+	32:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 9
+	},
+	33:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	34:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 10
+	},
+	35:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 0
+	},
+	36:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	37:{
+		"N" : False,
+		"E" : True,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	38:{
+		"N" : True,
+		"E" : False,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	39:{
+		"N" : True,
+		"E" : False,
+		"S" : True,
+		"W" : False,
+		"item" : 0
+	},
+	40:{
+		"N" : False,
+		"E" : False,
+		"S" : True,
+		"W" : True,
+		"item" : 18
+	},
+	41:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	42:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : False,
+		"item" : 0
+	},
+	43:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 19
+	},
+	44:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 11
+	},
+	45:{
+		"N" : False,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	46:{
+		"N" : True,
+		"E" : True,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	47:{
+		"N" : True,
+		"E" : False,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+	48:{
+		"N" : True,
+		"E" : False,
+		"S" : False,
+		"W" : True,
+		"item" : 0
+	},
+}
 
 def target_finder(board, target):
 	for i in board:
