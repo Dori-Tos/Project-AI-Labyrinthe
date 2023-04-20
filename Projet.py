54bcbb5db3bec2c931e4f598f4e0b574128d248c
Dorian Smaelen <dorian.smaelen@hotmail.com>
2023-04-20 11:35:29 +0200

    added tile_turner

--- Projet.py
+++ Projet.py
@@ -16,7 +16,7 @@ current = 0
 remaining = [4, 4]
 positions = [0, 48]
 
-target = 3
+targets = [3,13]
 
 tile={
 	"N" : True,
@@ -377,51 +377,51 @@ def new_board(board,tile,place):
 	#pre le board et la tile en plus (tile)
 	#post le board après mouvement et la nouvelle tile libre
 	temp=tile
-	if place==1:
+	if place == "A":
 		tile=board[43]
 		board.update({43:board.get(36),36:board.get(29),29:board.get(22),22:board.get(15),15:board.get(8),8:board.get(1),1:temp})
 	
-	elif place==3:
+	elif place == "B":
 		tile=board[45]
 		board.update({45:board.get(38),38:board.get(31),29:board.get(24),24:board.get(17),17:board.get(10),10:board.get(3),3:temp})
 	
-	elif place==5:
+	elif place == "C":
 		tile=board[47]
 		board.update({47:board.get(40),40:board.get(33),33:board.get(26),26:board.get(19),19:board.get(12),12:board.get(5),5:temp})
 	
-	elif place==7:
+	elif place == "L":
 		tile=board[13]
 		board.update({13:board.get(12),12:board.get(11),11:board.get(10),10:board.get(9),9:board.get(8),8:board.get(7),7:temp})
 
-	elif place==21:
+	elif place == "K":
 		tile=board[27]
 		board.update({27:board.get(26),26:board.get(25),25:board.get(24),24:board.get(23),23:board.get(22),22:board.get(21),21:temp})
 	
-	elif place==35:
+	elif place == "J":
 		tile=board[41]
 		board.update({41:board.get(40),40:board.get(39),39:board.get(38),38:board.get(37),37:board.get(36),36:board.get(35),35:temp})
 
-	elif place==13:
+	elif place == "D":
 		tile=board[7]
 		board.update({7:board.get(8),8:board.get(9),9:board.get(10),10:board.get(11),11:board.get(12),12:board.get(13),13:temp})
 
-	elif place==27:
+	elif place == "E":
 		tile=board[21]
 		board.update({21:board.get(22),22:board.get(23),23:board.get(24),24:board.get(25),25:board.get(26),26:board.get(27),27:temp})
 
-	elif place==41:
+	elif place == "F":
 		tile=board[35]
 		board.update({35:board.get(36),36:board.get(37),37:board.get(38),38:board.get(39),39:board.get(40),40:board.get(41),41:temp})
 	
-	elif place==43:
+	elif place == "I":
 		tile=board[1]
 		board.update({1:board.get(8),8:board.get(15),15:board.get(22),22:board.get(29),29:board.get(36),36:board.get(43),43:temp})
 
-	elif place==45:
+	elif place == "H":
 		tile=board[3]
 		board.update({3:board.get(10),10:board.get(17),17:board.get(24),24:board.get(31),31:board.get(38),38:board.get(45),45:temp})
 
-	elif place==47:
+	elif place == "G":
 		tile=board[5]
 		board.update({5:board.get(12),12:board.get(19),19:board.get(26),26:board.get(33),33:board.get(40),40:board.get(47),47:temp})
 	return board,tile
@@ -488,6 +488,20 @@ def successors(node, board):
 			res.append(dx)
 	return res
 
+def tile_turner(tile, rotation): # tourne de 90° degrés vers la droite autant de fois que le nbr rotation le demande
+	if rotation == 0:
+		return tile
+	else:
+		n = 0
+		while n < rotation:
+			temp = dict(tile)
+			tile.update({"E": temp.get("N")})
+			tile.update({"S": temp.get("E")})
+			tile.update({"W": temp.get("S")})
+			tile.update({"N": temp.get("W")})
+			n += 1
+	return tile
+
 from collections import deque
 
 def timeit(fun):
@@ -542,7 +556,7 @@ def BFS(start, target, board, tile, place, iteration):
 #print(BFS(0, target, board, tile, None, iteration))
 iteration = 0
 
-def winner(remaining, current, players):
+def winner(remaining, current):
 	other_nbr = (current+1)%2
 	if remaining[current] == 0:
 		return current
@@ -551,20 +565,20 @@ def winner(remaining, current, players):
 	else:
 	    return None
 	
-def gameOver(remaining, current, players):
-	if winner(remaining, current, players) is not None:
+def gameOver(remaining, current):
+	if winner(remaining, current) is not None:
 		return True
 	else:
 		return None
 
 def heuristic(remaining, player): # permet de dire à l'ia si le jeu s'arrête
-	if gameOver(remaining, current, players):
-		theWinner = winner(remaining, current, players)
+	if gameOver(remaining, current):
+		theWinner = winner(remaining, current)
 		if theWinner is None:
 			return 0
 		if theWinner == player:
-			return 1
-		return -1
+			return 9
+		return -9
 
 def moves(board, treasure_remaining): # nécessairee si on veut un peu de random
 	res = []						  # + à changer si on veut que ça marche
@@ -575,14 +589,15 @@ def moves(board, treasure_remaining): # nécessairee si on veut un peu de random
 	random.shuffle(res)
 	return res
 
-def negamaxWithPruning(positions, target, board, remaining, current, players, tile, alpha=float('-inf'), beta=float('inf')):
+def negamaxWithPruning(positions, targets, board, remaining, current, players, tile, alpha=float('-inf'), beta=float('inf')):
+	target = targets[current]
 	other_nbr = (current+1)%2
 	start = positions[current]
-	if gameOver(remaining, current, players) == True:
+	if gameOver(remaining, current,) == True:
 		return -heuristic(remaining, current), None
 
 	theValue, theMove = float('-inf'), None
-	"for node in moves(board, remaining[current_nbr]):"
+	#for node in moves(board, remaining[current_nbr]):
 	while True:
 		q = deque()
 		q.append(start)
@@ -598,23 +613,27 @@ def negamaxWithPruning(positions, target, board, remaining, current, players, ti
 					parents[successor] = node
 					q.append(successor)
 		
-		actions = deque()
+		action = None
 
 		if q == []:
-			places = [1, 3, 5, 7, 13, 21, 27, 35, 41, 43, 45, 47]
+			rotations = [0, 1, 2, 3]
+			rotation = random.choice(rotations)
+			tile = tile_turner(tile, rotation)
+			places = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
 			place = random.choice(places)
-			actions.append(place)
+			action = place
 			board, tile = new_board(board, tile, place)
 
-		value, _ = negamaxWithPruning(positions, target, board, remaining, (current+1)%2, players, tile, -beta, -alpha)
+		value, _ = negamaxWithPruning(positions, targets, board, remaining, (current+1)%2, players, tile, -beta, -alpha)
 		if value > theValue:
 			theValue, theMove = value, node
 		alpha = max(alpha, theValue)
 		if alpha >= beta:
 			break
-	return -theValue, theMove
+	last_pos = q.popleft()
+	return last_pos, tile, action
 
-print(negamaxWithPruning(positions, target, board, remaining, current, players, tile))
+print(negamaxWithPruning(positions, targets, board, remaining, current, players, tile))
 
 def the_move_played():
     pass
