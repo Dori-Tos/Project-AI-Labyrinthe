c3c9f17a8099d437c21faa5d294cdc3f4b6b80fe
Maxime Thiry Faulkner <2235@ecam.be>
2023-05-07 09:20:21 +0100

    Try to fix the connection to server

--- Network_functions.py
+++ Network_functions.py
@@ -16,7 +16,9 @@ def inscription(address, request, port, name, matricules):
         s.connect(address)
         s.send(json.dumps({"request":request, "port": port, "name": name, "matricules": matricules}).encode())
         print({"request":"subscribe", "port": port, "name": name, "matricules": matricules})
+
 def receiver(serverAddress, address):
+    print("We are in receiver")
     #while True: #pour les tests mock du socket et faire une fonction prossess/ le while doit etre en dehors sinon cela envoie plusieurs fois la meme chose
     with socket.socket() as s:
         s.bind(serverAddress)
@@ -34,13 +36,18 @@ def receiver(serverAddress, address):
             lives = received.get("lives")
             errors = received.get("errors")
             state = received.get("state")
-            Util_fonctions.random_moves(state.get("board"),state.get("tile"),state.get("positions"))
+            with socket.socket() as s:
+                s.connect(address)
+                move=Util_fonctions.random_moves(state.get("board"),state.get("tile"),state.get("positions"))
+                s.send(json.dumps({"response": "move","move": move,"message": "Fun message"}).encode())
+                print("played")
+
 def receiver2(serverAddress, address):
     #while True: #pour les tests mock du socket et faire une fonction prossess / le while doit etre en dehors sinon cela envoie plusieurs fois la meme chose
         with socket.socket() as s:
             s.bind(serverAddress)
             s.listen()
-            client, notaddress = s.accept() 
+            client, address = s.accept() 
             received = json.loads(client.recv(10000).decode()) #str?
             print(received)
             process_receiver(received)
@@ -56,6 +63,11 @@ def process_receiver(received,address):
         lives = received.get("lives")
         errors = received.get("errors")
         state = received.get("state")
+        with socket.socket() as s:
+            s.connect(address)
+            move=Util_fonctions.random_moves(state.get("board"),state.get("tile"),state.get("positions"))
+            s.send()
+            print("played")
 
 def ping_pong(address):
     with socket.socket() as s:
