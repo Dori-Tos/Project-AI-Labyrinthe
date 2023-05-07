0f84683e029f9e676c2d585b60d903706a3f8184
Maxime Thiry Faulkner <2235@ecam.be>
2023-05-07 16:00:24 +0100

    rearranged receiver and created proccess_receiver

--- Network_functions.py
+++ Network_functions.py
@@ -3,8 +3,7 @@ import threading
 import json
 import Util_fonctions
 port = 6942
-serverAddress = ('localhost', 6942)
-#address = ('172.17.10.59', 3000)
+serverAddress = ('localhost', port)
 address=('localhost',3000)
 request = "subscribe"
 name = "AI_of_the_dead"
@@ -18,41 +17,18 @@ def inscription(address, request, port, name, matricules):
         print({"request":"subscribe", "port": port, "name": name, "matricules": matricules})
 
 def receiver(serverAddress, address):
-    print("We are in receiver")
-    #while True: #pour les tests mock du socket et faire une fonction prossess/ le while doit etre en dehors sinon cela envoie plusieurs fois la meme chose
-    with socket.socket() as s:
-        s.bind(('localhost', 6942))
-        s.listen()
-        client, address = s.accept() 
-        received = json.loads(client.recv(100000).decode())
-        print(received)
-        if received.get("response")=="ok":
-            print("Successful inscription")
-        elif received.get("response")=="error":
-            print(received.get("error"))
-        elif received.get("request")=="ping":
-            return ping_pong(address)
-        elif received.get("request")=="play":
-            lives = received.get("lives")
-            errors = received.get("errors")
-            state = received.get("state")
-            with socket.socket() as s:
-                s.connect(address)
-                move=Util_fonctions.random_moves(state.get("board"),state.get("tile"),state.get("positions"))
-                s.send(json.dumps({"response": "move","move": move,"message": "Fun message"}).encode())
-                print("played")
-
-def receiver2(serverAddress, address):
     #while True: #pour les tests mock du socket et faire une fonction prossess / le while doit etre en dehors sinon cela envoie plusieurs fois la meme chose
         with socket.socket() as s:
-            s.bind(serverAddress)
+            print("We are in receiver")
+            s.bind(address)
             s.listen()
             client, address = s.accept() 
             received = json.loads(client.recv(10000).decode()) #str?
             print(received)
-            process_receiver(received)
+            process_receiver(received,address)
 
 def process_receiver(received,address):
+    print("We are in proccess receiver")
     if received.get("response")=="ok":
         print("Successful inscription")
     elif received.get("response")=="error":
@@ -63,18 +39,20 @@ def process_receiver(received,address):
         lives = received.get("lives")
         errors = received.get("errors")
         state = received.get("state")
-        with socket.socket() as s:
+        random_move(address)
+            
+def random_move(address):
+    with socket.socket() as s:
             s.connect(address)
             move=Util_fonctions.random_moves(state.get("board"),state.get("tile"),state.get("positions"))
             s.send(json.dumps({"response": "move","move": move,"message": "Fun message"}))
             print("played")
-            
 
 def ping_pong(address):
     with socket.socket() as s:
         s.connect(('localhost',3000))
         s.send(json.dumps({"response": "pong"}).encode())
-        print("pong")
+        print({"response": "pong"})
 
 def giveup(address):
     with socket.socket() as s:
